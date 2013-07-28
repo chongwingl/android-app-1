a56021d079bba353265d2dfdb6d43c7cfa3ad81c
laetitia chong-wing <chongwingl@gmail.com>
2013-07-28 19:58:09 +0200

    Fichiers de gestion

--- gen/com/project/aidememoire/R.java
+++ gen/com/project/aidememoire/R.java
@@ -14,13 +14,15 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int menu_settings=0x7f070001;
+        public static final int peopleListView=0x7f070000;
     }
     public static final class layout {
-        public static final int activity_aide_memoire=0x7f030000;
+        public static final int main_activity=0x7f030000;
+        public static final int test=0x7f030001;
     }
     public static final class menu {
-        public static final int activity_aide_memoire=0x7f060000;
+        public static final int main_activity=0x7f060000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
