40316a54b358d3d8993dbf4a26010135aa5147ae
laetitia chong-wing <chongwingl@gmail.com>
2014-01-01 20:03:20 +0100

    Deletion of the nested fragment

--- src/com/project/aidememoire/fragment/HomeFragment.java
+++ src/com/project/aidememoire/fragment/HomeFragment.java
@@ -7,14 +7,15 @@ import android.app.Activity;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.RadioGroup;
 
 import com.project.aidememoire.R;
 import com.project.aidememoire.adapter.PersonListAdapter;
@@ -23,30 +24,49 @@ import com.project.aidememoire.model.Person;
 
 public class HomeFragment extends Fragment {
 	
-	private final static String TAG = "AccueilFragment";
+	private final static String TAG = "HomeFragment";
 	
 	private View fragmentView;
 	private DataBaseAdapter mDbHelper;
 	private List<Person> persons;
 	private ListView peopleListView;
 	private PersonListAdapter adapter;
-	private FragmentTransaction  transaction;
-	private Fragment addElementFragment;
+	
+	private Button addButton;
+	private EditText nameEdit;
+	private EditText surnameEdit;
+	private EditText sumEdit;
+	private EditText dateEdit;
+	private RadioGroup sumSignsRadioGroup;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 
-		fragmentView = inflater.inflate(R.layout.accueil_fragment, container, false);
+		fragmentView = inflater.inflate(R.layout.home_fragment, container, false);
 		peopleListView = (ListView) fragmentView.findViewById(R.id.peopleListView);
 		persons = getPersons();
 
 		adapter = new PersonListAdapter(getActivity(), R.layout.person_list, persons);
 		peopleListView.setAdapter(adapter);
 		
-		addElementFragment = new AddElementFragment();
-		transaction = getChildFragmentManager().beginTransaction();
-		transaction.add(R.id.addElementFragment, addElementFragment).commit();
+		addButton = (Button) fragmentView.findViewById(R.id.addElement);
+		
+		nameEdit = (EditText) fragmentView.findViewById(R.id.name);
+		surnameEdit = (EditText) fragmentView.findViewById(R.id.surname);
+		sumEdit = (EditText) fragmentView.findViewById(R.id.sum);
+		dateEdit = (EditText) fragmentView.findViewById(R.id.date);
+		
+		sumSignsRadioGroup = (RadioGroup) fragmentView.findViewById(R.id.sumSign);
+		
+		addButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	Log.i(TAG, "name : " + nameEdit.getText());
+            	Log.i(TAG, "surname : " + surnameEdit.getText());
+            	Log.i(TAG, "sum : " + sumEdit.getText());
+            	Log.i(TAG, "date : " + dateEdit.getText());
+            }
+        });
 
 		return fragmentView;
 	}
