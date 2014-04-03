c2bd74aa44e4b4430375fa7823dfdf1e0ba1d845
l.chong-wing <l.chong-wing@novedia-solutions.com>
2014-04-03 12:07:44 +0200

    Use databaseapi method to get data from database

--- src/com/project/aidememoire/fragment/ListFragment.java
+++ src/com/project/aidememoire/fragment/ListFragment.java
@@ -1,15 +1,14 @@
 package com.project.aidememoire.fragment;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import com.project.aidememoire.R;
 import com.project.aidememoire.adapter.PersonListAdapter;
 import com.project.aidememoire.adapter.database.DataBaseAdapter;
+import com.project.aidememoire.adapter.database.api.DatabaseApi;
 import com.project.aidememoire.listener.OnFragmentChange;
 import com.project.aidememoire.model.Person;
 
-import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -27,60 +26,29 @@ public class ListFragment extends Fragment implements OnFragmentChange{
 	private PersonListAdapter adapter;
 	
 	private View fragmentView;
-	private DataBaseAdapter mDbHelper;
+	private DatabaseApi dataBaseApi;
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
+		dataBaseApi = new DatabaseApi(getActivity());
+		
 		fragmentView = super.onCreateView(inflater, container, savedInstanceState);
 		
 		fragmentView = inflater.inflate(R.layout.list_layout, container, false);
 		
 		peopleListView = (ListView) fragmentView.findViewById(R.id.peopleListView);
-		persons = getPersons();
+		persons = dataBaseApi.fetchAllPerson();
 
 		adapter = new PersonListAdapter(getActivity(), R.layout.person_list, persons);
 		peopleListView.setAdapter(adapter);
 		
 		return fragmentView;
 	}
-	
-	public List<Person> getPersons(){
-		Cursor c = this.getData();
-		List<Person> personRetrieved = new ArrayList<Person>();
-
-		while (c.moveToNext()){
-			personRetrieved.add(new Person(c.getString(1), c.getString(2)));
-			Log.i(TAG, "People : " + c.getString(0) + " " + c.getString(1) + " " + c.getString(2));
-		}
-		mDbHelper.close();
-		return personRetrieved;
-		
-	}
-	
-	public Cursor getData(){
-		mDbHelper = new DataBaseAdapter(this.getActivity());
-		mDbHelper.open();
-
-		if(mDbHelper.fetchPerson("Martin", "Pierre").getCount() == 0){
-			mDbHelper.addCreditLine("Martin", "Pierre", 1234567890, 1234);
-		}
-		
-		if(mDbHelper.fetchPerson("Durand", "Pauline").getCount() == 0){
-			mDbHelper.addCreditLine("Durand", "Pauline", 987654321, 765);	
-		}
-		
-		if(mDbHelper.fetchPerson("Dupont", "Paul").getCount() == 0){
-			mDbHelper.addDetteLine("Dupont", "Paul", 1235679, 987);
-		}
-		
-		return mDbHelper.fetchAllPersons();
-		
-	}
 
 	@Override
 	public void OnFragmentVisible() {
-		persons = getPersons();
+		Log.i(TAG, "Fragment visible");
+		persons = dataBaseApi.fetchAllPerson();
 
 		if(persons.size() > adapter.getCount()){
 			List<Person> temp = persons;
@@ -90,5 +58,4 @@ public class ListFragment extends Fragment implements OnFragmentChange{
 	}
 	
 	
-
 }
