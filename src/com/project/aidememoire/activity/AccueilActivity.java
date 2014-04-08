3ec2620f90cefc277cfe50c3b78df9028366eacf
l.chong-wing <l.chong-wing@novedia-solutions.com>
2014-04-08 15:34:41 +0200

    Add fragment dynamically to main activity

--- src/com/project/aidememoire/activity/AccueilActivity.java
+++ src/com/project/aidememoire/activity/AccueilActivity.java
@@ -1,20 +1,32 @@
 package com.project.aidememoire.activity;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 
 import com.project.aidememoire.R;
+import com.project.aidememoire.fragment.ListFragment;
 
 public class AccueilActivity extends FragmentActivity {
 	
 	private final static String TAG = "AideMemoireActivity";
 
+	private FragmentManager fragmentManager = getSupportFragmentManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         setContentView(R.layout.main);
+        
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        
+        ListFragment fragment = new ListFragment();
+        fragmentTransaction.add(R.id.main_container, fragment, "Liste");
+        fragmentTransaction.commit();
+
     }
     
     @Override
