67a971f220f144037e2609310edc01404c725f52
l.chong-wing <l.chong-wing@novedia-solutions.com>
2014-04-08 15:12:04 +0200

    The main activity displays a fragment

--- src/com/project/aidememoire/activity/AccueilActivity.java
+++ src/com/project/aidememoire/activity/AccueilActivity.java
@@ -1,53 +1,25 @@
 package com.project.aidememoire.activity;
 
-import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.view.ViewPager;
 import android.view.Menu;
 
 import com.project.aidememoire.R;
-import com.project.aidememoire.adapter.TabsAdapter;
-import com.project.aidememoire.fragment.AddFragment;
-import com.project.aidememoire.fragment.ListFragment;
 
 public class AccueilActivity extends FragmentActivity {
 	
 	private final static String TAG = "AideMemoireActivity";
-	
-	private TabsAdapter tabsAdapter;
-	private ViewPager viewPager;
-	private FragmentManager fragmentManager = getSupportFragmentManager();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-        viewPager = new ViewPager(this);
-        viewPager.setId(R.id.viewpager);
-        setContentView(viewPager);
-        
-        ActionBar bar = getActionBar();
-        bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-        bar.setDisplayOptions(ActionBar.DISPLAY_SHOW_TITLE);
-        
-        tabsAdapter = new TabsAdapter(this, fragmentManager, viewPager);
-        tabsAdapter.addTab(bar.newTab().setText(R.string.tab_list),
-        		ListFragment.class, null);
-        tabsAdapter.addTab(bar.newTab().setText(R.string.tab_add),
-        		AddFragment.class, null);
-        
-        if (savedInstanceState != null) {
-            bar.setSelectedNavigationItem(savedInstanceState.getInt("tab", 0));
-        }
-
+        setContentView(R.layout.main);
     }
     
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putInt("tab", getActionBar().getSelectedNavigationIndex());
     }
 
     @Override
