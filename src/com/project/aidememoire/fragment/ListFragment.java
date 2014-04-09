046a6f9ed5b9e1e2a5b6b3658eec0216f648d90f
l.chong-wing <l.chong-wing@novedia-solutions.com>
2014-04-09 11:06:23 +0200

    Change header and footer style

--- src/com/project/aidememoire/fragment/ListFragment.java
+++ src/com/project/aidememoire/fragment/ListFragment.java
@@ -19,6 +19,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class ListFragment extends Fragment{
@@ -27,6 +28,8 @@ public class ListFragment extends Fragment{
 	
 	private ListView peopleListView;
 	private PersonListAdapter adapter;
+	
+	private Button addButton;
 	
 	private View fragmentView;
 	private DatabaseApi dataBaseApi;
@@ -40,41 +43,45 @@ public class ListFragment extends Fragment{
 		fragmentView = inflater.inflate(R.layout.list_layout, container, false);
 		
 		peopleListView = (ListView) fragmentView.findViewById(R.id.personListView);
+		addButton = (Button) fragmentView.findViewById(R.id.footer_button);
 
 		adapter = new PersonListAdapter(getActivity(), dataBaseApi.fetchAllPersonAndMoneyCursor(), false);
 		peopleListView.setAdapter(adapter);
+
 
 		peopleListView.setOnItemClickListener(new OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position,
                     long id) {
-	             if(position != 0 && position != (adapter.getCount() + 1)){
-	            	 	Cursor c = (Cursor) adapter.getItem(position-1);
-	            	 	// 0: _id
-	            	 	// 1: p_id
-	            	 	// 2: date
-	            	 	// 3: montant
-	            	 	// 4: type
-	            	 	// 5: _id
-	            	 	// 6: name
-	            	 	// 7: surname
-	            	 	
-	            	 	Person person = new Person(c.getString(6), c.getString(7), new Money(c.getInt(3), c.getString(2), c.getString(4)));
+        	 	Cursor c = (Cursor) adapter.getItem(position-1);
+        	 	// 0: _id
+        	 	// 1: p_id
+        	 	// 2: date
+        	 	// 3: montant
+        	 	// 4: type
+        	 	// 5: _id
+        	 	// 6: name
+        	 	// 7: surname
+        	 	
+        	 	Person person = new Person(c.getString(6), c.getString(7), new Money(c.getInt(3), c.getString(2), c.getString(4)));
 
-		          	   	if(dataBaseApi.deleteSomme(c)){
-	             	   		adapter.changeCursor(dataBaseApi.fetchAllPersonAndMoneyCursor());
-	            			adapter.notifyDataSetChanged();       			
-		          	   	} 
-	             }
-	             
-	             else if(position == (adapter.getCount() + 1)) {
-	            	 FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
-	            	 fragmentTransaction.replace(R.id.main_container, new AddFragment());
-	            	 fragmentTransaction.addToBackStack(null);
-	            	 fragmentTransaction.commit();
-	             }
+          	   	if(dataBaseApi.deleteSomme(c)){
+         	   		adapter.changeCursor(dataBaseApi.fetchAllPersonAndMoneyCursor());
+        			adapter.notifyDataSetChanged();       			
+          	   	}      
             }
         });
+		
+		addButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+	           	fragmentTransaction.replace(R.id.main_container, new AddFragment());
+	           	fragmentTransaction.addToBackStack(null);
+	           	fragmentTransaction.commit();
+			}
+		});
 		
 		return fragmentView;
 	}
